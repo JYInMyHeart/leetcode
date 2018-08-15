b81556dd8a2f37f3c5a0af425cbe63135999db1d
changhuxxu@gmail.com <changhuxxu@gmail.com>
2018-08-15 13:24:02 +0800

    TLE!!!

--- src/main/scala/array/AdvantageShuffle.scala
+++ src/main/scala/array/AdvantageShuffle.scala
@@ -1,29 +1,32 @@
 package array
 
+
+
 object AdvantageShuffle extends App {
   def advantageCount(A: Array[Int], B: Array[Int]): Array[Int] = {
     //TLE  i don't know how to optimize
+    import scala.collection.mutable.Stack
     var i = 0
-    val t = Array.ofDim[Int](A.length)
-    Array.copy(A,0,t,0,A.length)
+    val t = Stack[Int](A:_*)
     B.foreach(
       x => {
-        A(i) = if(t.exists(_ > x)) {
-          val temp = t.filter(_ > x).min
-          t(t.indexOf(temp)) = Int.MinValue
-          temp
-        } else{
-          val tt = t.filter(_ != Int.MinValue).min
-          t(t.indexOf(tt)) = Int.MinValue
-          tt
-        }
+        A(i) =
+          if (t.exists(_ > x)) {
+            val temp = t.filter(_ > x).min
+            t(t.indexOf(temp)) = Int.MinValue
+            temp
+          } else {
+            val tt = t.filter(_ != Int.MinValue).min
+            t(t.indexOf(tt)) = Int.MinValue
+            tt
+          }
         i += 1
       }
     )
     A
   }
 
-  val a = Array(2,0,4,1,2)
-  val b = Array(1,3,0,0,2)
-  advantageCount(a,b).foreach(print)
+  val a = Array(2, 0, 4, 1, 2)
+  val b = Array(1, 3, 0, 0, 2)
+  advantageCount(a, b).foreach(print)
 }
