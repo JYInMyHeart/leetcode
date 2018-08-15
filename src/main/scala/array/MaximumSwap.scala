cb307e7e6acca84fbb07529d184707b8e879355d
alex <Xuck.alex@gmail.com>
2018-08-15 22:43:20 +0800

    array

--- src/main/scala/array/MaximumSwap.scala
+++ src/main/scala/array/MaximumSwap.scala
@@ -1,14 +1,23 @@
 package array
 
-object MaximumSwap extends App{
+object MaximumSwap extends App {
   def maximumSwap(num: Int): Int = {
-    var t = num.toString
-    val max = t.max
-    val index = t.indexOf(max)
-    t = t.replaceFirst(max.toString,t.head.toString)
-    t = t.replaceFirst(t.head.toString,max.toString)
-    t.toInt
+    var t = num.toString.toCharArray
+    var tt = num.toString.sorted.reverse
+    var i = 0
+    var flag = true
+    while (flag && i < tt.length) {
+      if (t(i) != tt(i)) {
+        val pos = t(i)
+        t(i) = tt(i)
+        t(t.lastIndexOf(tt(i))) = pos
+        flag = false
+      }
+      i += 1
+    }
+    t.mkString.toInt
   }
 
-  println(maximumSwap(2736))
+  println(maximumSwap(98368))
+  println(maximumSwap(1993))
 }
