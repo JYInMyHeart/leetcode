0496cce69c5c18e7c287bc042efa397945465f4b
昌堃 <changhuxxu@gmail.com>
2018-08-17 12:46:13 +0800

    ac  hard!

--- src/main/scala/array/NumberofMatchingSubsequences.scala
+++ src/main/scala/array/NumberofMatchingSubsequences.scala
@@ -2,23 +2,32 @@ package array
 
 object NumberofMatchingSubsequences extends App {
   def numMatchingSubseq(S: String, words: Array[String]): Int = {
-    var res = 0
-    for(c <- words){
-      var count = 0
-      for(cc <- c){
-        for(s <- S){
-          if(cc == s){
-            count += 1
-          }
+    def compareStr(l:String,r:String) = {
+      var i = 0
+      var j = 0
+      val ll = l.toCharArray
+      var rr = r.toCharArray
+      while(i < rr.length && j < ll.length){
+        if(ll(j) == rr(i)){
+          i += 1
+          j += 1
+        }else{
+          j += 1
         }
       }
-      if(count == c.length){
-        res += 1
+      if(i == rr.length) true else false
+    }
+    var count = 0
+    for(s <- words){
+      if(compareStr(S,s)){
+        count += 1
       }
     }
-    res
+    count
   }
+
   val a = Array("a", "bb", "acd", "ace")
   val s = "abcde"
+//  println(compareStr(s, "ace"))
   println(numMatchingSubseq(s, a))
 }
---
+++ src/main/scala/array/TrappingRainWater.scala
@@ -0,0 +1,41 @@
+package array
+
+object TrappingRainWater extends App {
+  def trap(height: Array[Int]): Int = {
+    if(height.isEmpty) return 0
+    var left = 0
+    var right = 0
+    val n = height.length
+    var i = 0
+    var max = 0
+    while(i <= height.indexOf(height.max)){
+      if(height(i) > height(left)){
+        val t = (for(index <- left + 1 until i) yield height(index)).sum
+        max += (i - left - 1) * height(left) - t
+        left = i
+      }
+      i += 1
+    }
+    val hs = height.reverse
+    i = 0
+    left = 0
+    right = 0
+    while(i <= hs.indexOf(hs.max)){
+      if(hs(i) > hs(left)){
+        val t = (for(index <- left + 1 until i) yield hs(index)).sum
+        max += (i - left - 1) * hs(left) - t
+        left = i
+      }
+      i += 1
+    }
+    left = height.indexOf(height.max)
+    right = height.lastIndexOf(height.max)
+    if(left != right){
+      val t = (for(index <- left + 1 until right) yield height(index)).sum
+      max += height.max * (right - 1 - left) - t
+    }
+    max
+  }
+
+  println(trap(Array(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)))
+}
