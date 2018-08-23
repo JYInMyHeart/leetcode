bf881e7b1afe44479b696b55b675dd901969d9cb
alex <changhuxxu@gmail.com>
2018-08-23 13:02:29 +0800

    Update JewelsandStones.scala

--- src/main/scala/array/JewelsandStones.scala
+++ src/main/scala/array/JewelsandStones.scala
@@ -2,6 +2,16 @@ package array
 
 object JewelsandStones extends App {
   def numJewelsInStones(J: String, S: String): Int = {
-
-  }
+        val j = J.toCharArray
+      val s = S.toCharArray
+      var count = 0
+      for(i <- j){
+        for(k <- s){
+          if(i == k){
+            count += 1
+          }
+        }
+      }
+      count
+    }
 }
