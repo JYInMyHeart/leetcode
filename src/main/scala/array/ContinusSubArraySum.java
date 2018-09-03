37d09dca0689959476afb3def9741ffdc9edb3d8
changhuxxu@gmail.com <changhuxxu@gmail.com>
2018-09-03 22:45:51 +0800

    maybe i should't learn dp!

--- src/main/scala/array/ContinusSubArraySum.java
+++ src/main/scala/array/ContinusSubArraySum.java
@@ -1,18 +1,19 @@
-public class ContinusSubArraySum{
-  
+package array;
+import java.util.HashMap;
+import java.util.Map;
 
-public boolean checkSubarraySum(int[] nums, int k) {
-    Map<Integer, Integer> map = new HashMap<Integer, Integer>(){{put(0,-1);}};;
-    int runningSum = 0;
-    for (int i=0;i<nums.length;i++) {
-        runningSum += nums[i];
-        if (k != 0) runningSum %= k; 
-        Integer prev = map.get(runningSum);
-        if (prev != null) {
-            if (i - prev > 1) return true;
+public class ContinusSubArraySum {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>() {{ put(0, -1); }};
+        int runningSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            runningSum += nums[i];
+            if (k != 0) runningSum %= k;
+            Integer prev = map.get(runningSum);
+            if (prev != null) {
+                if (i - prev > 1) return true;
+            } else map.put(runningSum, i);
         }
-        else map.put(runningSum, i);
+        return false;
     }
-    return false;
 }
-    }
