95888bab7d5575347460d7f7d8202d7bcbf55d2d
alex <changhuxxu@gmail.com>
2018-09-08 11:11:34 +0800

    some problems are difficult to understand meanings.

--- src/main/scala/dp/SoupServings.scala
+++ src/main/scala/dp/SoupServings.scala
@@ -3,6 +3,7 @@ package dp
 object SoupServings {
   def soupServings(N: Int): Double = {
     def M(i: Int) = math.max(i, 0)
+
     var n = N
     n = n / 25 + (if (n % 25 > 0) 1 else 0)
     if (n >= 500) return 1.0
@@ -12,7 +13,7 @@ object SoupServings {
       var i: Int = 0
       while (i <= n) {
         val j: Int = s - i
-        if (j >= 0 && j <= n){
+        if (j >= 0 && j <= n) {
           var ans: Double = 0.0
           if (i == 0) ans = 1.0
           if (i == 0 && j == 0) ans = 0.5
@@ -25,5 +26,20 @@ object SoupServings {
       s += 1
     }
     memo(n)(n)
+  }
+
+  val memo = Array.ofDim[Double](200, 200)
+
+  def soupServings1(N: Int): Double =
+    if (N >= 4800) 1.0
+    else f((N + 24) / 25, (N + 24) / 25)
+
+  def f(a: Int, b: Int): Double = {
+    if (a <= 0 && b <= 0) return 0.5
+    if (a <= 0) return 1
+    if (b <= 0) return 0
+    if (memo(a)(b) > 0) return memo(a)(b)
+    memo(a)(b) = 0.25 * (f(a - 4, b) + f(a - 3, b - 1) + f(a - 2, b - 2) + f(a - 1, b - 3))
+    memo(a)(b)
   }
 }
