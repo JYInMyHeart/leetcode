d271c5978845fae137d835c9dcb5adb422c9ceb6
alex <changhuxxu@gmail.com>
2018-09-16 22:00:37 +0800

    easy problems

--- src/main/scala/array/FizzBuzz.scala
+++ src/main/scala/array/FizzBuzz.scala
@@ -2,7 +2,7 @@ package array
 
 object FizzBuzz extends App {
   def fizzBuzz(n: Int): List[String] = {
-    Stream.iterate(1)(_ + 1).takeWhile(_ > n).map {
+    Stream.iterate(1,n)(_ + 1).map {
       case y if y % 3 == 0 && y % 5 == 0 => "FuzzBuzz"
       case y if y % 3 == 0 => "Fuzz"
       case y if y % 5 == 0 => "Buzz"
