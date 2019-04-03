84fc7065d97b65821e937c766aa952ea3544cb6a
alex <changhuxxu@gmail.com>
2019-04-03 23:40:50 +0800

    :globe_with_meridians: Internationalization and localization.

--- src/main/scala/array/WordSearch.java
+++ src/main/scala/array/WordSearch.java
@@ -10,18 +10,27 @@ import java.util.Map;
  */
 public class WordSearch {
     public boolean exist(char[][] board, String word) {
-        Map<Character,Integer> map = new HashMap<>();
-        for(char[] chars:board){
-            for(char c:chars){
-                map.put(c,map.getOrDefault(c,0) + 1);
+        char[] w = word.toCharArray();
+        for (int y=0; y<board.length; y++) {
+            for (int x=0; x<board[y].length; x++) {
+                if (exist(board, y, x, w, 0)) return true;
             }
         }
-        for (char c:word.toCharArray()){
-            if(map.get(c) == 0 || map.get(c) == null)
-                return false;
-            if(map.containsKey(c))
-                map.put(c,map.get(c) - 1);
-        }
-        return true;
+        return false;
     }
+
+    private boolean exist(char[][] board, int y, int x, char[] word, int i) {
+        if (i == word.length) return true;
+        if (y<0 || x<0 || y == board.length || x == board[y].length) return false;
+        if (board[y][x] != word[i]) return false;
+        board[y][x] ^= 256;
+        boolean exist = exist(board, y, x+1, word, i+1)
+                || exist(board, y, x-1, word, i+1)
+                || exist(board, y+1, x, word, i+1)
+                || exist(board, y-1, x, word, i+1);
+        board[y][x] ^= 256;
+        return exist;
+    }
+
+
 }
