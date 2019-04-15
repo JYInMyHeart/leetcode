bf892f99f2380fa6548fe448b59515bf47015d8a
alex <changhuxxu@gmail.com>
2019-04-15 23:52:24 +0800

    ordered count

--- src/main/scala/array/MedianofTwoSortedArrays.scala
+++ src/main/scala/array/MedianofTwoSortedArrays.scala
@@ -14,29 +14,24 @@ object MedianofTwoSortedArrays extends App{
     if(list.size % 2 == 0) (list(list.size / 2) + list(list.size / 2 - 1)).toDouble / 2 else list(list.size / 2).toDouble
   }
 
-  def findMedianSortedArrays1(nums1: Array[Int], nums2: Array[Int]): Double = {
-    //some failed attempts
-//    var n1 = 0.0
-//    var n2 = 0.0
-//
-//    while(){
-//      var mid1 = nums1.length / 2
-//      var mid2 = nums2.length
-//      if(nums1.nonEmpty) {
-//        n1 = if(nums1.length % 2 == 0) (nums1(nums1.length / 2) + nums1(nums1.length / 2 - 1)).toDouble / 2 else nums1(nums1.length / 2).toDouble
-//      }
-//      if(nums2.nonEmpty) {
-//        n2 = if(nums2.length % 2 == 0) (nums2(nums2.length / 2) + nums2(nums2.length / 2 - 1)).toDouble / 2 else nums2(nums2.length / 2).toDouble
-//      }
-//      if(n1 > n2){
-//
-//      }
-//    }
-//
-//    if(nums1.isEmpty) n2
-//    else if(nums2.isEmpty) n1
-//    else (n1 + n2) / 2
-    0.0
+  def findMedianSortedArrays1(A: Array[Int], B: Array[Int]) = {
+    val m = A.length
+    val n = B.length
+    val l = (m + n + 1) / 2
+    val r = (m + n + 2) / 2
+    (getkth(A, 0, B, 0, l) + getkth(A, 0, B, 0, r)) / 2.0
+  }
+
+  def getkth(A: Array[Int], aStart: Int, B: Array[Int], bStart: Int, k: Int): Double = {
+    if (aStart > A.length - 1) return B(bStart + k - 1)
+    if (bStart > B.length - 1) return A(aStart + k - 1)
+    if (k == 1) return Math.min(A(aStart), B(bStart))
+    var aMid = Integer.MAX_VALUE
+    var bMid = Integer.MAX_VALUE
+    if (aStart + k / 2 - 1 < A.length) aMid = A(aStart + k / 2 - 1)
+    if (bStart + k / 2 - 1 < B.length) bMid = B(bStart + k / 2 - 1)
+    if (aMid < bMid) getkth(A, aStart + k / 2, B, bStart, k - k / 2) // Check: aRight + bLeft
+    else getkth(A, aStart, B, bStart + k / 2, k - k / 2) // Check: bRight + aLeft
   }
 
 }
