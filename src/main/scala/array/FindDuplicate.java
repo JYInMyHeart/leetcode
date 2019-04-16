19dc95dfe5187ce7ab05b506467f86ec885a29e3
alex <changhuxxu@gmail.com>
2019-04-17 00:31:57 +0800

    slow and fast!

--- src/main/scala/array/FindDuplicate.java
+++ src/main/scala/array/FindDuplicate.java
@@ -1,5 +1,6 @@
 package array;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,9 +12,27 @@ import java.util.Set;
 public class FindDuplicate {
     public int findDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        for(int i = 0;i < nums.length;i++){
-            if(set.contains(nums[i])) return nums[i];
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) return nums[i];
             else set.add(nums[i]);
+        }
+        return -1;
+    }
+
+    public int findDuplicate1(int[] nums) {
+        if (nums.length > 1) {
+            int slow = nums[0];
+            int fast = nums[nums[0]];
+            while (fast != slow) {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+            fast = 0;
+            while (fast != slow) {
+                fast = nums[fast];
+                slow = nums[slow];
+            }
+            return slow;
         }
         return -1;
     }
