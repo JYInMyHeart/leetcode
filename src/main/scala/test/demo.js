0a6c729b1735a5e00d4cc9d77043a4bac8889bb0
alex <changhuxxu@gmail.com>
2019-12-08 23:42:04 +0800

    :camera_flash: Adding or updating snapshots.

---
+++ src/main/scala/test/demo.js
@@ -0,0 +1,8 @@
+var a = { name: "2", address: "s", a: "" };
+var b;
+b = a;
+var people;
+var s = people.firstName;
+var s = people.next.firstName;
+var s = people.next.next.firstName;
+var s = people.next.next.next.firstName;
