c7f00b19f41a00136ee2562de797a5a89f97e732
alex <changhuxxu@gmail.com>
2019-12-21 21:45:18 +0800

    :art: Improving structure / format of the code.

---
+++ src/main/scala/string/SplitaStringinBalancedStrings.java
@@ -0,0 +1,29 @@
+package string;
+
+/**
+ * @Author: xck
+ * @File: SplitaStringinBalancedStrings
+ * @Time: 21:44 2019/12/21
+ */
+public class SplitaStringinBalancedStrings {
+    public int balancedStringSplit(String s) {
+        int slow = 0;
+        int fast = 0;
+        int i = 0;
+        int count = 0;
+        while(slow < s.length() && fast < s.length() && i < s.length()){
+            if(s.charAt(i) == 'L'){
+                slow++;
+            }else if(s.charAt(i) == 'R'){
+                fast++;
+            }
+
+            if(slow == fast && slow > 0){
+                count++;
+                slow = fast = 0;
+            }
+            i++;
+        }
+        return count;
+    }
+}
