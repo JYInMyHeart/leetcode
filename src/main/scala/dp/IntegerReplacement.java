6995a6e4436f939c1b43a7999186e27ab96f69bc
alex <changhuxxu@gmail.com>
2019-12-26 16:33:21 +0800

    :art: Improving structure / format of the code.

---
+++ src/main/scala/dp/IntegerReplacement.java
@@ -0,0 +1,34 @@
+package dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: xck
+ * @File: IntegerReplacement
+ * @Time: 16:25 2019/12/26
+ */
+public class IntegerReplacement {
+
+    Map<Integer, Integer> map = new HashMap<>();
+
+    {
+        map.put(1, 0);
+        map.put(2, 1);
+    }
+
+    public int integerReplacement(int n) {
+        if (map.get(n) != null) {
+            return map.get(n);
+        }
+        if (n % 2 == 0) {
+            int t = integerReplacement(n / 2) + 1;
+            map.put(n, t);
+            return t;
+        } else {
+            int t = Math.min(integerReplacement(n / 2 + 1), integerReplacement(n / 2)) + 2;
+            map.put(n, t);
+            return t;
+        }
+    }
+}
