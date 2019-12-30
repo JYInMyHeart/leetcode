a8f4c48b767741c5d683fe8b8da0fe1927c8d1be
alex <changhuxxu@gmail.com>
2019-12-30 17:33:08 +0800

    :art: Improving structure / format of the code.

---
+++ src/main/scala/string/SmallestSubsequence.java
@@ -0,0 +1,34 @@
+package string;
+
+import java.util.Stack;
+
+/**
+ * @Author: xck
+ * @File: SmallestSubsequence
+ * @Time: 16:55 2019/12/30
+ */
+public class SmallestSubsequence {
+
+    public String smallestSubsequence(String S) {
+        Stack<Integer> stack = new Stack<>();
+        int[] last = new int[26], seen = new int[26];
+        for (int i = 0; i < S.length(); ++i) {
+            last[S.charAt(i) - 'a'] = i;
+        }
+        for (int i = 0; i < S.length(); ++i) {
+            int c = S.charAt(i) - 'a';
+            if (seen[c]++ > 0) {
+                continue;
+            }
+            while (!stack.isEmpty() && stack.peek() > c && i < last[stack.peek()]) {
+                seen[stack.pop()] = 0;
+            }
+            stack.push(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i : stack) {
+            sb.append((char) ('a' + i));
+        }
+        return sb.toString();
+    }
+}
