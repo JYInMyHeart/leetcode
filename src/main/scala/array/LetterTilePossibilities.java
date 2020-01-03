34c0b3466d3d3a9e40b45751c2e9a38ad2dbdc08
alex <changhuxxu@gmail.com>
2020-01-03 22:27:05 +0800

    :green_apple: finished 1079

--- src/main/scala/array/LetterTilePossibilities.java
+++ src/main/scala/array/LetterTilePossibilities.java
@@ -11,8 +11,16 @@ import java.util.Set;
  * @Time: 22:07 2020/1/3
  */
 public class LetterTilePossibilities {
-    List<String> set = new ArrayList<>();
+
+    Set<String> set = new HashSet<>();
     boolean[] vis;
+
+    public static void main(String[] args) {
+        LetterTilePossibilities l = new LetterTilePossibilities();
+        l.numTilePossibilities("AB");
+//        [A, AA, AAB, AABB, AABBA, AABBAB]
+    }
+
     public int numTilePossibilities(String tiles) {
         int n = tiles.length();
         vis = new boolean[n];
@@ -20,22 +28,23 @@ public class LetterTilePossibilities {
         System.out.println(set);
         return set.size();
     }
-    void dfs(StringBuilder sb, String tiles){
-        if(sb.length()>0) set.add(sb.toString());
-        if(sb.length()>=tiles.length()) return;
-        for(int i=0;i<tiles.length();i++){
-            if(vis[i]) continue;
+
+    public void dfs(StringBuilder sb, String tiles) {
+        if (sb.length() > 0) {
+            set.add(sb.toString());
+        }
+        if (sb.length() >= tiles.length()) {
+            return;
+        }
+        for (int i = 0; i < tiles.length(); i++) {
+            if (vis[i]) {
+                continue;
+            }
             vis[i] = true;
             int len = sb.length();
             dfs(sb.append(tiles.charAt(i)), tiles);
             sb.setLength(len);
             vis[i] = false;
         }
-    }
-
-    public static void main(String[] args) {
-        LetterTilePossibilities l = new LetterTilePossibilities();
-        l.numTilePossibilities("AB");
-//        [A, AA, AAB, AABB, AABBA, AABBAB]
     }
 }
