16d234b24af99434ddb5faedd9c731830bb141b1
alex <changhuxxu@gmail.com>
2020-01-12 20:14:43 +0800

    :green_apple: finished 221

--- src/main/scala/string/MaximalSquare.java
+++ src/main/scala/string/MaximalSquare.java
@@ -43,5 +43,20 @@ public class MaximalSquare {
         }
         return max * max;
     }
+
+    public int maximalSquareBydp(char[][] matrix) {
+        if(matrix.length == 0) return 0;
+        int max = 0;
+        int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
+        for(int i = 1;i <= matrix.length;i++){
+            for(int j = 1;j <= matrix[0].length;j++){
+                if(matrix[i - 1][j - 1] == '1'){
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j],dp[i][j - 1]),dp[i - 1][j - 1]) + 1;
+                    max = Math.max(dp[i][j],max);
+                }
+            }
+        }
+        return max * max;
+    }
 }
 
